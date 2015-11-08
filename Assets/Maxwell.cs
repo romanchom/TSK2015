4f2f3d20e6172aced97211cc3be9537a89071965
romanchom <romanchom@gmail.com>
2015-11-08 14:12:31 +0100

    Symulacja trochę, tak jakby, być może działa.

--- Assets/Maxwell.cs
+++ Assets/Maxwell.cs
@@ -11,15 +11,23 @@ public class Maxwell : MonoBehaviour {
 	scalar[,] u_r;
 	scalar[,] e_r;
 
-	//const scalar e_0 = 8.854187817e-12F;
-	//const scalar u_0 = 1.2566370614e-6F;
+	Color[] texData;
+
+	const double e_0 = 8.854187817e-12;
+	const double u_0 = 1.2566370614e-6;
 
 	[SerializeField]
 	uint size = 128;
 	[SerializeField]
-	scalar timeStep = 1e-30F;
+	double timeStep = 1e-27F;
 	[SerializeField]
-	scalar worldScale = 1e-9F;
+	double worldScale = 1e-9F;
+
+	Texture2D texture;
+	RenderTexture tex;
+
+	[SerializeField]
+	Material material;
 
 	// Use this for initialization
 	void Start () {
@@ -29,8 +37,11 @@ public class Maxwell : MonoBehaviour {
 		u_r = new scalar[size, size];
 
 		for (uint x = 0; x < size; ++x) {
+			float X = (float) x - 128;
+			X *= 0.1f;
+			float v = Mathf.Exp(-X * X);
 			for (uint y = 0; y < size; ++y) {
-				H[x, y] = 0;
+				H[x, y] = v;
 				u_r[x, y] = 1;
 			}
 		}
@@ -44,31 +55,52 @@ public class Maxwell : MonoBehaviour {
 				e_r[x, y] = 1;
 			}
 		}
+
+		texData = new Color[size * size];
+
+		texture = new Texture2D((int) size, (int) size, TextureFormat.RFloat, false);
+		material.mainTexture = texture;
+		UpdateTex();
 	}
 	
+	void UpdateTex() {
+		for(uint i = 0; i < size; ++i) {
+			for(uint j = 0; j < size; ++j) {
+				texData[i * size + j].r = H[i, j];
+			}
+		}
+		texture.SetPixels(texData);
+		texture.Apply();
+	}
+
 	// Update is called once per frame
 	void Update () {
 		uint sizeMM = size - 1;
-		scalar stepOverScale = timeStep / worldScale;
+		double stepOverScale = timeStep / worldScale;
+		scalar stepOverScaleOverU_0 = (scalar) (stepOverScale / u_0);
+		scalar stepOverScaleOverE_0 = (scalar)(stepOverScale / e_0);
 
-		for(uint x = 0; x < size; ++x) {
+		for (uint x = 0; x < size; ++x) {
 			for(uint y = 0; y < size; ++y) {
 				scalar de_xOverDy = E[x, y + 1].x - E[x, y].x;
 				scalar de_yOverDx = E[x + 1, y].y - E[x, y].y;
 				scalar dBdt = de_yOverDx - de_xOverDy;
-				scalar deltaH = dBdt * stepOverScale / u_r[x, y];
+				scalar deltaH = dBdt * stepOverScaleOverE_0 / u_r[x, y];
 				H[x, y] += deltaH;
 			}
 		}
+
 
 		for (uint x = 0; x < sizeMM; ++x) {
 			for (uint y = 0; y < sizeMM; ++y) {
 				scalar db_zOverDx = H[x + 1, y] - H[x, y];
 				scalar db_zOverDy = H[x, y + 1] - H[x, y];
-				vector dDdt = new vector(db_zOverDy, -db_zOverDx);
-				vector deltaE = dDdt * stepOverScale / e_r[x + 1, y + 1];
+				vector dDdt = new vector(-db_zOverDy, db_zOverDx);
+				vector deltaE = dDdt * stepOverScaleOverU_0 / e_r[x + 1, y + 1];
 				E[x + 1, y + 1] += deltaE;
 			}
 		}
+
+		UpdateTex();
 	}
 }
---
+++ Assets/TextureUnlitBicolor.shader
@@ -0,0 +1,60 @@
+﻿Shader "Unlit/TextureUnlitBicolor"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_ColorPos ("Positive Color", Color) = (1, 0, 0, 0)
+		_ColorNeg ("Negative Color", Color) = (0, 1, 0, 0)
+		_Scale ("Scale", Float) = 1 
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float4 _ColorPos;
+			float4 _ColorNeg;
+			float _Scale;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				float col = tex2D(_MainTex, i.uv).r;
+				col *= _Scale;
+				return max(col, 0) * _ColorPos + max(-col, 0) * _ColorNeg;
+			}
+			ENDCG
+		}
+	}
+}
